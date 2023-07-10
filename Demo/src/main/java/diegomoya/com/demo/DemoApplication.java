06610bdf0f269dc7f77b9687bdc3c1d4b3d25fcf
stfudiego <stfudiegomv@gmail.com>
2023-07-10 18:56:54 -0500

    Initial commit

---
+++ Demo/src/main/java/diegomoya/com/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package diegomoya.com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello client! How are you doing?";
+	}
+	
+	@RequestMapping("/random")
+	public String random() {
+		return "Spring Boot is great! So easy to just respond with strings";
+	}
+}
